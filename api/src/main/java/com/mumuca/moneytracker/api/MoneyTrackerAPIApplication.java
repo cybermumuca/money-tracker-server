f3f3b280590f78bf924d84b7029a9863e2a03d3e
sx1n <samuellaurindodelima2004@gmail.com>
2025-01-16 10:26:12 -0300

    Initial Commit

---
+++ api/src/main/java/com/mumuca/moneytracker/api/MoneyTrackerAPIApplication.java
@@ -0,0 +1,13 @@
+package com.mumuca.moneytracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackerAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTrackerAPIApplication.class, args);
+	}
+
+}
