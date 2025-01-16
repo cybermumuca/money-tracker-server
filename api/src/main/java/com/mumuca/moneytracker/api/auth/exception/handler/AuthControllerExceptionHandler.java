c2a1bd84d8e97335cd9420c6924475f86cba630b
sx1n <samuellaurindodelima2004@gmail.com>
2025-01-16 12:41:36 -0300

    feat: handle UserAlreadyExistsException

---
+++ api/src/main/java/com/mumuca/moneytracker/api/auth/exception/handler/AuthControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.mumuca.moneytracker.api.auth.exception.handler;
+
+import com.mumuca.moneytracker.api.auth.controller.AuthController;
+import com.mumuca.moneytracker.api.auth.exception.UserAlreadyExistsException;
+import com.mumuca.moneytracker.api.exception.dto.APIErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.mumuca.moneytracker.api.util.HttpUtils.buildErrorResponse;
+
+@RestControllerAdvice(assignableTypes = {AuthController.class})
+public class AuthControllerExceptionHandler {
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<APIErrorResponse<String>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return buildErrorResponse(
+                HttpStatus.CONFLICT,
+                "User already exists.",
+                "The user already exists."
+        );
+    }
+}
