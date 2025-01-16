2abcaffa868412c2a93193e9d31a5533f2df2645
sx1n <samuellaurindodelima2004@gmail.com>
2025-01-16 10:39:21 -0300

    feat: add User and Role

---
+++ api/src/main/java/com/mumuca/moneytracker/api/auth/model/Role.java
@@ -0,0 +1,28 @@
+package com.mumuca.moneytracker.api.auth.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "authority")
+    private String authority;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Set<User> users;
+}
---
+++ api/src/main/java/com/mumuca/moneytracker/api/auth/model/User.java
@@ -0,0 +1,58 @@
+package com.mumuca.moneytracker.api.auth.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+import java.util.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Column(insertable = false, updatable = false)
+    private Set<Role> roles;
+
+    @Column(name = "password")
+    private String password;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private Instant createdDate = Instant.now();
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private Instant lastModifiedDate = Instant.now();
+
+    public User(String id) {
+        this.id = id;
+    }
+}
